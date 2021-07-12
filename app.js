1d92bdb754ef12652fe8371be720804a0dce3c1c
Antony Gaetano <antonygaetano1000@gmail.com>
2021-07-12 17:50:20 -0300

    .....

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
-/*i = 1;
+i = j = 1;
 function Slide(){
-    var div1 = document.getElementById("img-Laços");
+    var div1 = document.getElementById("img-Lacos");
     var div2 = document.getElementById("img-Tiara");
 
     div1.style.backgroundImage="url(imagens/lacos"+i+".jpg)";
+    
+     div2.style.backgroundImage="url(imagens/tiara"+j+".jpg)";
     i++;
+    j++;
+    if(i==5){
+     i = 0;
+    }
+
+    if(j == 6){
+        j = 0;
+    }
 }
 
 function Add(){
-    setInterval(Slide,1000);
+   setInterval(Slide,2500);
     
 }
 
-window.addEventListener("load",Add); */
+window.addEventListener("load",Add); 
